43be958fed6cb056abc52891d0986861369b1460
Ilya Golovin <74474615+ILScc@users.noreply.github.com>
2022-05-31 21:07:17 +0300

    feat: add ability to skip vendor-prefixed rules (#10)

--- src/getShortcuts.ts
+++ src/getShortcuts.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode'
+import { getExtensionSetting } from 'vscode-framework'
 import { rules } from '@unocss/preset-wind'
 
 export default (position: vscode.Position, document: vscode.TextDocument) => {
@@ -9,6 +10,19 @@ export default (position: vscode.Position, document: vscode.TextDocument) => {
         .filter(rule => typeof rule[0] === 'string' && typeof rule[1] === 'object')
         .map(([shortcut, rule]) => {
             const rules = (Array.isArray(rule) ? rule : Object.entries(rule))
+                .filter(([prop, value], i, rulesArr) => {
+                    if (getExtensionSetting('skipVendorPrefix') === 'none') return true
+                    const hasUnvendored = (current: string, matchUnvendored: (vendorLength: number, unvendored: string) => boolean) => {
+                        const match = /^-\w+-/.exec(current)
+                        if (!match) return false
+                        return matchUnvendored(match[0]!.length, current.slice(match[0]?.length))
+                    }
+
+                    return !(
+                        hasUnvendored(prop, (vendorLength, unvendored) => rulesArr.some(([rule]) => unvendored === rule.slice(vendorLength)))
+                        // hasUnvendored(value as string, match => rulesArr.some(([, value]) => (value as string).slice(match[0]!.length)))
+                    )
+                })
                 .map(([prop, value]) => {
                     if (typeof value === 'number') value = `${value.toString()}px`
                     return `${prop}: ${value!};`
