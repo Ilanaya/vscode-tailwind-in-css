f309aa6dbce2042b9c4cbae0b3018a260cb0d78d
Ilya Golovin <me@ilscc.ru>
2022-07-23 23:00:13 +0300

    fix tests

--- src/parseCss.test.ts
+++ src/parseCss.test.ts
@@ -29,7 +29,14 @@ test('works with incorrect syntax', () => {
     const pos = str.indexOf('|')
     const strToParse = str.slice(0, pos) + str.slice(pos + 1)
     const { usedRules } = parseCss(strToParse, pos)
-    expect(usedRules).toMatchInlineSnapshot('Map {}')
+    expect(usedRules).toMatchInlineSnapshot(`
+    Map {
+        "display" => {
+          "offset": 13,
+          "value": "flex",
+        },
+      }
+    `)
 })
 test('throws with incorrent syntax', () => {
     const str = /* scss*/ `
@@ -39,30 +46,5 @@ test('throws with incorrent syntax', () => {
 }`
     const pos = str.indexOf('|')
     const strToParse = str.slice(0, pos) + str.slice(pos + 1)
-    expect(parseCss(strToParse, pos)).not.toThrowErrorMatchingSnapshot(`
-    {
-      "column": 5,
-      "endColumn": 9,
-      "endLine": 4,
-      "input": {
-        "column": 5,
-        "endColumn": 9,
-        "endLine": 4,
-        "line": 4,
-        "source": "
-    .test {
-        display: flex;
-        flex
-    }",
-      },
-      "line": 4,
-      "reason": "Unknown word",
-      "setMessage": "Function<setMessage>",
-      "showSourceCode": "Function<showSourceCode>",
-      "source": "
-    .test {
-        display: flex;
-        flex
-    }",
-    }`)
+    expect(parseCss(strToParse, pos)).not.toThrowErrorMatchingSnapshot(`"expected is not a function"`)
 })
